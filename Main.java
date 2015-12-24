cd4de1088fe04f0ca17f6e6766b5ae530750757b
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2015-12-24 12:40:57 +0100

    main

---
+++ Main.java
@@ -0,0 +1,32 @@
+package org.max.concurrent;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * foo
+ * bar
+ */
+public class Main implements Runnable {
+  private volatile int x = 0;
+  // private AtomicInteger y = new AtomicInteger();
+
+  public static void main(String[] args) throws InterruptedException {
+    Main main = new Main();
+    for (int i = 0; i < 2; ++i) {
+      new Thread(main).start();
+    }
+    Thread.sleep(500);
+    // System.out.println(main.y.get());
+    System.out.println(main.x);
+  }
+
+  @Override
+  public void run() {
+    // System.out.println("in");
+    for (int i = 0; i < 100000; i++) {
+      // y.incrementAndGet();
+      x++;
+    }
+    // System.out.println("out");
+  }
+}
