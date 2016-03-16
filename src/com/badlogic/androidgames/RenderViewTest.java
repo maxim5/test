816f09f34699961b255aa677458249d17585872a
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2016-03-16 19:12:28 +0100

    render

--- src/com/badlogic/androidgames/RenderViewTest.java
+++ src/com/badlogic/androidgames/RenderViewTest.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+
+// Comments
 public class RenderViewTest extends Activity {
     class RenderView extends View {
         Random rand = new Random();
