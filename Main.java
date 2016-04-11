45686fae6ba19a9136cee6788aca4936e6c4f099
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2016-04-11 16:28:21 +0200

    feature change

--- Main.java
+++ Main.java
@@ -3,31 +3,25 @@ package org.max.concurrent;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * foo
  * bar
  */
 public class Main implements Runnable {
   private volatile int x = 0;
   // private AtomicInteger y = new AtomicInteger();
 
-  public static void main(String[] args) throws InterruptedException {
+  // Comment
+  public static void main(String[] args) throws Exception {
     Main main = new Main();
     for (int i = 0; i < 2; ++i) {
       new Thread(main).start();
     }
-    Thread.sleep(500);
+    Thread.sleep(1500);
     // System.out.println(main.y.get());
-    System.out.println(main.x);
+    System.out.println("x=" + main.x);
   }
 
-//
-//
-//
-//
-//
-
   @Override
-  public void run() {   // edit
+  public void run() {
     //
     // System.out.println("in");
     for (int i = 0; i < 100000; i++) {
