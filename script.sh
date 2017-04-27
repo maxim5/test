3edf91b333d8dc21673ff229025e0b49324968be
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-04-27 17:57:00 +0200

    drop more files

--- script.sh
+++
@@ -1,2 +0,0 @@
-# creates a number of branches
-for i in {2..5}; do git checkout master && git checkout -b brach$i && echo "? $i" >> README.md && git add README.md && git commit -m "update README $i" && git push origin HEAD:branch$i; done;
